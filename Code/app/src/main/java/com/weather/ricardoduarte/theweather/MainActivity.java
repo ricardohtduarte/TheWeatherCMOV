a58c8eafa34dae2ecdfc841f0a5017bc378e0b28
ricardohtduarte <ricardohtduarte@gmail.com>
2018-02-18 22:51:19 +0000

    Created city list with images

--- Code/app/src/main/java/com/weather/ricardoduarte/theweather/MainActivity.java
+++ Code/app/src/main/java/com/weather/ricardoduarte/theweather/MainActivity.java
@@ -3,12 +3,18 @@ package com.weather.ricardoduarte.theweather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.weather.ricardoduarte.theweather.weatherService.Model.City;
 import com.weather.ricardoduarte.theweather.weatherService.Service.WeatherClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,14 +26,20 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     TextView city;
+    ListView city_list;
     String base_url =  "http://api.apixu.com/v1/";
     String api_key = "07e921a3441f4e148e201522181702";
+    List<City> cities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        city = findViewById(R.id.city_id);
+        create_listview();
+    }
+
+
+    private void build_retrofit(){
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl(base_url)
                 .addConverterFactory(GsonConverterFactory.create());
@@ -39,9 +51,7 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<City>() {
             @Override
             public void onResponse(Call<City> call, Response<City> response) {
-
                 Log.i("Test", new Gson().toJson(response));
-
             }
 
             @Override
@@ -50,13 +60,23 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "error :(", Toast.LENGTH_SHORT).show();
             }
         });
-
-        String text = "comes from resquest";
-        changeTextView(city, text);
     }
 
-    private void changeTextView(TextView city, String text){
-        city.setText(text);
+    private void request_city(String name){
 
+    }
+
+
+    private void create_listview(){
+        City city1 = new City("Paris", 21);
+        City city2 = new City("Lisbon", 24);
+
+        cities = new ArrayList<>();
+        cities.add(city1);
+        cities.add(city2);
+
+        ListAdapter city_list_adapter = new ListCityAdapter(this, cities);
+        city_list = findViewById(R.id.city_list);
+        city_list.setAdapter(city_list_adapter);
     }
 }
--- Code/app/src/main/java/com/weather/ricardoduarte/theweather/weatherService/Model/City.java
+++ Code/app/src/main/java/com/weather/ricardoduarte/theweather/weatherService/Model/City.java
@@ -10,13 +10,24 @@ public class City {
     String country;
     float lat;
     float lon;
+    float temperature;
 
-    public City(String name, String region, String country, float lat, float lon) {
+    public City(String name, float temperature) {
+        this.name = name;
+        this.temperature = temperature;
+    }
+
+    public City(String name, String region, String country, float lat, float lon, float temperature) {
         this.name = name;
         this.region = region;
         this.country = country;
         this.lat = lat;
         this.lon = lon;
+        this.temperature = temperature;
+    }
+
+    public float getTemperature() {
+        return temperature;
     }
 
     public String getName() {
